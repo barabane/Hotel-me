0bc8ece1ed7541f4995771eaa6dae58c6e59b6e3
barabane <barabayne@gmail.com>
2024-07-25 10:51:44 +0500

    test: init testing

--- app/database.py
+++ app/database.py
@@ -3,12 +3,16 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from config import settings
 
-DB_URL = settings.DATABASE_URL
+if settings.MODE == "TEST":
+    DB_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DB_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
 
-engine = create_async_engine(DB_URL)
-
-DATABASE_PARAMS = {"poolclass": NullPool}
-engine_nullpool = create_async_engine(settings.DATABASE_URL, **DATABASE_PARAMS)
+engine = create_async_engine(url=DB_URL, **DATABASE_PARAMS)
+engine_nullpool = create_async_engine(
+    url=settings.DATABASE_URL, **{"poolclass": NullPool})
 
 async_session_maker = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False)
