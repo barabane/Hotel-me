bbe22584bae506d2abb7829d1939c1118df2056c
barabane <barabayne@gmail.com>
2024-08-02 20:01:15 +0500

    refactor: Templates
    Add new templates
    Update templates
    Update pages router

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,5 +1,9 @@
+from datetime import datetime, UTC
 from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
+from app.users.exceptions import TokenExpiredException
+from jose import JWTError
+from app.utils.decode_jwt import decode_jwt
 
 from app.hotels.router import get_hotel
 
@@ -14,3 +18,17 @@ templates = Jinja2Templates(directory="app/templates")
 @router.get("/hotels")
 async def get_hotels_page(request: Request, hotels=Depends(get_hotel)):
     return templates.TemplateResponse(name="hotels.html", context={"request": request, "hotels": hotels})
+
+
+@router.get("/recover")
+async def recover_password_page(request: Request, access_token: str):
+    if not access_token:
+        raise TokenExpiredException
+    try:
+        payload = decode_jwt(access_token)
+    except JWTError:
+        raise TokenExpiredException
+    expire: str = payload.get("exp")
+    if (not expire) or expire < datetime.now(UTC).timestamp():
+        raise TokenExpiredException
+    return templates.TemplateResponse(name="recover.html", context={"request": request, "access_token": access_token})
